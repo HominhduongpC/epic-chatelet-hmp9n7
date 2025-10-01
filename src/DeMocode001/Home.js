959222818c4b982dca3d43822fece21b6071f5b5
HominhduongpC <chutichho75@gmail.com>
2025-10-01 16:42:09 +0700

    Initial commit

---
+++ src/DeMocode001/Home.js
@@ -0,0 +1,14 @@
+const Home = () => {
+  var day = 10;
+  var month = 10;
+  var str = "k18 Học đại học gia định";
+  var dssv = ["Nguyễn Văn An, Trần Văn Ba, Nguyễn Văn Cường "];
+  var ds2 = [
+    { id: "001 ", hoten: "", lop: "" },
+    { id: "002 ", hoten: "", lop: "" },
+    { id: "003", hoten: "", lop: "" },
+  ];
+
+  return <div>xin chào k18 Đại Học Gia Định</div>;
+};
+export default Home;
